1a606a99cb37371f615480a4b6627e2dfbbc00c5
Wojciech <wojciechrusin1@gmail.com>
2019-12-30 01:00:41 +0100

    After first day project. Todo: connect with db

---
+++ README.md
@@ -0,0 +1,25 @@
+#Name app
+CookBook Blog. 
+
+## Description
+Create web app with using microframework Flask. In this application we will be able to create your accounts
+ and add new posts.
+ 
+##Installation
+Use the Terminal in PyCharm
+```bash
+pip install flask
+```
+```terminal
+pip install flask-wtf
+```
+```terminal
+pip install sqlalchemy
+```
+SECRET_KEY
+use in cmd
+```cmd
+python
+import secrets
+secrets.token_hex(16)
+```
---
+++ flaskapp.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from CookBook.forms import RegistrationForm, LoginForm
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '4002cf852e0aebbb86e1d3c7b8cb384a'
+
+posts = [
+    {
+        'author': 'Corey Shafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+
+]
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'asd':
+            flash('You have been loggedin', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
